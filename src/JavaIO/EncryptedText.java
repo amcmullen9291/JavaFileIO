00cd90e2550cdf7c8556089468b643ffb76df153
Anthony <amcmullen9291@gmail.com>
2021-11-03 16:32:07 -0400

    put original message and encryption in separate files

--- src/JavaIO/EncryptedText.java
+++ src/JavaIO/EncryptedText.java
@@ -1,4 +1,16 @@
 package JavaIO;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class EncryptedText {
+
+    static JavaFilesIO hiddenMessage;
+
+    public static void main(String[] args) {
+        System.out.println(hiddenMessage.message);
+        System.out.println();
+        System.out.println(hiddenMessage.message2);
+
+    }
+
 }
--- src/JavaIO/JavaFilesIO.java
+++ src/JavaIO/JavaFilesIO.java
@@ -5,11 +5,16 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class JavaFilesIO {
+    static String message = "This input is from JavaFilesIO.java ";
+    static String message2 = "This text is also from JavaFilesIO.java";
 
     public static void main(String[] args) {
+
         try {
             BufferedWriter fileWriter = new BufferedWriter(new FileWriter("JavaFile.txt"));
-            fileWriter.write("This input is from JavaFilesIO.java");
+            fileWriter.write(message);
+            fileWriter.write(System.getProperty( "line.separator" ));
+            fileWriter.write(message2);
             fileWriter.close();
         }catch(IOException e ){
             e.printStackTrace();
