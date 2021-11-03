223000d71d03ca742d22392e6d0496fda4dd8f84
Anthony <amcmullen9291@gmail.com>
2021-11-03 15:31:27 -0400

    import BufferedWriter and FileWriter

---
+++ src/JavaIO/JavaFilesIO.java
@@ -0,0 +1,17 @@
+package JavaIO;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class JavaFilesIO {
+
+    public static void main(String[] args) {
+        try {
+            BufferedWriter fileWriter = new BufferedWriter(new FileWriter("JavaFile.txt"));
+            fileWriter.write("This input is from JavaFiles.IO.java");
+        }catch(IOException e ){
+            e.printStackTrace();
+        }
+    }
+}
