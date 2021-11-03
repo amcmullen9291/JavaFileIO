e7f2b0ef1a4a395c41f9c692491c7ac303fcf5de
Anthony <amcmullen9291@gmail.com>
2021-11-03 15:35:07 -0400

    safety commit

--- src/JavaIO/JavaFilesIO.java
+++ src/JavaIO/JavaFilesIO.java
@@ -9,7 +9,7 @@ public class JavaFilesIO {
     public static void main(String[] args) {
         try {
             BufferedWriter fileWriter = new BufferedWriter(new FileWriter("JavaFile.txt"));
-            fileWriter.write("This input is from JavaFiles.IO.java");
+            fileWriter.write("This input is from JavaFilesIO.java");
             fileWriter.close();
         }catch(IOException e ){
             e.printStackTrace();
