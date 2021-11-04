3c8937044adc527755c3f4f3f5ade3a3d2ca52b0
Anthony <amcmullen9291@gmail.com>
2021-11-04 09:30:52 -0400

    safety commit

--- src/JavaIO/JavaFilesIO.java
+++ src/JavaIO/JavaFilesIO.java
@@ -3,17 +3,32 @@ package JavaIO;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class JavaFilesIO {
-    static String message = "This input is from JavaFilesIO.java ";
+    static String message = "This input is from JavaFilesIO.java";
     static String message2 = "This text is also from JavaFilesIO.java";
 
     public static void main(String[] args) {
+        System.out.println("Double digit key");
+        Scanner response = new Scanner(System.in);
+        String firstResponse = response.next();
+        System.out.println("Second key");
+        Scanner response2 = new Scanner(System.in);
+        String secondResponse = response2.next();
+        System.out.println("Enter message");
+        Scanner response3 = new Scanner(System.in);
+        String  yourMessage = response3.nextLine();
+
         try {
             BufferedWriter fileWriter = new BufferedWriter(new FileWriter("JavaFile.txt"));
-            fileWriter.write(message);
+            fileWriter.write(message+ firstResponse);
             fileWriter.write(System.getProperty( "line.separator" ));
-            fileWriter.write(message2);
+            fileWriter.write(message2+secondResponse);
+            fileWriter.write(System.getProperty( "line.separator" ));
+            fileWriter.write("------------------------------");
+            fileWriter.write(System.getProperty( "line.separator" ));
+            fileWriter.write(yourMessage);
             fileWriter.close();
         }catch(IOException e ){
             e.printStackTrace();
