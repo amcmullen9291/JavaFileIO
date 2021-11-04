1e7d8d3bb4aa226e357ac7bbde665589cb1c0035
Anthony <amcmullen9291@gmail.com>
2021-11-04 10:23:51 -0400

    reverse original message

--- src/JavaIO/JavaFilesIO.java
+++ src/JavaIO/JavaFilesIO.java
@@ -3,6 +3,8 @@ package JavaIO;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class JavaFilesIO {
@@ -19,6 +21,22 @@ public class JavaFilesIO {
         System.out.println("Enter message");
         Scanner response3 = new Scanner(System.in);
         String  yourMessage = response3.nextLine();
+        char[] originalMessage = yourMessage.toCharArray();
+        char[] newOriginalMessage = new char[originalMessage.length];
+        for(int i=0; i<originalMessage.length; i++){
+            char c = originalMessage[i];
+            c+=1;
+            newOriginalMessage[i] = c;
+        }
+        String reverseMessage = String. valueOf(newOriginalMessage);
+        StringBuilder input1 = new StringBuilder();
+
+        // append a string into StringBuilder input1
+        input1.append(reverseMessage);
+
+        // reverse StringBuilder input1
+        input1.reverse();
+
 
         try {
             BufferedWriter fileWriter = new BufferedWriter(new FileWriter("JavaFile.txt"));
@@ -28,7 +46,15 @@ public class JavaFilesIO {
             fileWriter.write(System.getProperty( "line.separator" ));
             fileWriter.write("------------------------------");
             fileWriter.write(System.getProperty( "line.separator" ));
-            fileWriter.write(yourMessage);
+            fileWriter.write(newOriginalMessage);
+            fileWriter.write(System.getProperty( "line.separator" ));
+            fileWriter.write(String.valueOf(input1));
+
+            // temp original message below for reference
+            fileWriter.write(System.getProperty( "line.separator" ));
+            fileWriter.write(originalMessage);
+
+
             fileWriter.close();
         }catch(IOException e ){
             e.printStackTrace();
